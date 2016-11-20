8089a87175463cc3a82ed000bd4b5c44280cdf64
trrapp@gmail.com <trrapp@gmail.com>
2016-11-19 18:00:03 -0700

    refactor to split out functions, add functionality for change state on menu, add menu with options, create object with theme arrays, connect everything

--- scripts/animation.js
+++ scripts/animation.js
@@ -3,54 +3,76 @@ let themes = {
     "url('images/astronomy/1.jpg')", "url('images/astronomy/2.jpg')", "url('images/astronomy/3.jpg')", "url('images/astronomy/4.jpeg')"
   ],
   code: [
-    "url('images/code/1.jpeg')",
+    "url('images/code/1.png')",
     "url('images/code/2.jpg')",
     "url('images/code/3.jpg')",
     "url('images/code/4.png')",
 ],
   matrix: [
-    "url('images/code/1.png')",
-    "url('images/code/2.jpg')",
-    "url('images/code/3.jpg')",
+    "url('images/matrix/1.png')",
+    "url('images/matrix/2.jpg')",
+    "url('images/matrix/3.jpg')",
   ],
+};
+
+let currentTheme = themes.astronomy;
+
+let switches = [document.getElementById("switch1"), document.getElementById("switch2"), document.getElementById("switch3"), document.getElementById("switch4"), document.getElementById("switch5"), document.getElementById("switch6"), document.getElementById("switch7"), document.getElementById("switch8")];
+
+let white = "url('images/white.jpeg')";
+
+let slategray = "url()";
+
+var pauseVar = 1;
+
+initializeThemeDropDown();
+
+setEventHandlers();
+
+function initializeThemeDropDown () {
+  let selectTag = document.getElementById("select")
+  for (var theme in themes) {
+    let option_tag = document.createElement("option");
+    option_tag.text = theme;
+
+    selectTag.add(option_tag)
+  };
+};
+
+function selectTheme () {
+  let selectThemeTag = document.getElementById("select");
+  currentTheme = themes[selectThemeTag.options[select.selectedIndex].value];
+  updateImages();
+};
+
+function setEventHandlers () {
+  document.getElementById("select").onchange = selectTheme;
 }
 
-let white = "url('images/white.jpeg')"
-
-var pauseVar = -1;
-console.log(pauseVar);
-
 function pause () {
+  pauseVar *= -1;
+  updateImages();
+};
 
+function updateImages () {
   switch(pauseVar) {
     case 1:
-    console.log("it got to this point");
 
     document.getElementById('pause').innerHTML = "<i class='fa fa-pause' aria-hidden='true'></i>";
 
-    console.log("it got to this point");
+    for (i = 0; i < switches.length; i = i+2) {
+      switches[i].style.backgroundImage = currentTheme[i];
+      switches[i+1].style.backgroundImage = white;
+    };
 
-    document.getElementById("switch1").style.backgroundImage = "url('images/photoone.jpg')";
-    document.getElementById("switch2").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch3").style.backgroundImage = "url('images/phototwo.jpg')";
-    document.getElementById("switch4").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch5").style.backgroundImage = "url('images/photothree.jpg')";
-    document.getElementById("switch6").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch7").style.backgroundImage = "url('images/photofour.jpg')";
-    document.getElementById("switch8").style.backgroundImage = "url('images/white.jpeg')";
     break;
     case -1:
     document.getElementById('pause').innerHTML = "<i class='fa fa-play' aria-hidden='true'></i>";
 
-    document.getElementById("switch1").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch2").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch3").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch4").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch5").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch6").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch7").style.backgroundImage = "url('images/white.jpeg')";
-    document.getElementById("switch8").style.backgroundImage = "url('images/white.jpeg')";
+    for (i = 0; i < switches.length; i++) {
+      switches[i].style.backgroundImage = white;
+    };
+
     break;
-  }
-  pauseVar *= -1;
+  };
 }
